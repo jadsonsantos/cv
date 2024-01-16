c477efbadffc0b0a7a5d04c005220abf3b0e0bae
Jadson Santos <jadsondossantos5@gmail.com>
2024-01-16 10:52:31 -0300

    hidden avatar on print mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Page() {
             </div>
           </div>
 
-          <Avatar className="h-28 w-28">
+          <Avatar className="h-28 w-28 print:hidden">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
